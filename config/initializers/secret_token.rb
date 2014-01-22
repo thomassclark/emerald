bd59024a0e60f83d95a526663d8dc6dd8f9c2031
Thomas Clark <thomassclark@me.com>
2014-01-22 22:02:21 +0000

    Created a rails application nd added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Emerald::Application.config.secret_key_base = '08d7d36ff791313d368082ceaa9e315038356e9b34e6120869ee02d3a12aa2b52655652e50f52d830a5de0c33cd014b0dc0acdf81db3349fe6fdd017ae63b4bd'
